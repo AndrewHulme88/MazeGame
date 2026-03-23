e98754005cc0efe5b22db1ea205e9c2c67b904f9
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2026-03-23 18:21:33 +1100

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,24 +4,63 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Animator animator;
 
     public int playerLives = 3;
 
     private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Vector2 lastMoveDirection = Vector2.down;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
     }
 
     private void Start()
     {
         FindFirstObjectByType<UIManager>().UpdateLives(playerLives);
+
+        animator.SetFloat("LastMoveX", lastMoveDirection.x);
+        animator.SetFloat("LastMoveY", lastMoveDirection.y);
+    }
+
+    private void Update()
+    {
+        moveInput = InputSystem.actions["Move"].ReadValue<Vector2>();
+
+        if(moveInput.sqrMagnitude > 0.01f)
+        {
+            if(Mathf.Abs(moveInput.x) > Mathf.Abs(moveInput.y))
+            {
+                moveInput = new Vector2(Mathf.Sign(moveInput.x), 0f);
+            }
+            else
+            {
+                moveInput = new Vector2(0f, Mathf.Sign(moveInput.y));
+            }
+
+            lastMoveDirection = moveInput;
+        }
+        else
+        {
+            moveInput = Vector2.zero;
+        }
+
+        animator.SetFloat("MoveX", moveInput.x);
+        animator.SetFloat("MoveY", moveInput.y);
+        animator.SetFloat("LastMoveX", lastMoveDirection.x);
+        animator.SetFloat("LastMoveY", lastMoveDirection.y);
+        animator.SetFloat("Speed", moveInput.sqrMagnitude);
     }
 
     private void FixedUpdate()
     {
-        Vector2 moveInput = InputSystem.actions["Move"].ReadValue<Vector2>();
         rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
     }
 
