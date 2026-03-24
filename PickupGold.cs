fcc04c3d28275f02a54c334e216564bb3671fbb6
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2026-03-24 19:53:09 +1100

    Add files via upload

--- PickupGold.cs
+++ PickupGold.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class PickupGold : MonoBehaviour
 {
     [SerializeField] private int goldAmount = 100;
+    [SerializeField] private GameObject pickupEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             GameManager.Instance.AddGold(goldAmount);
+
+            if (pickupEffect != null)
+            {
+                Instantiate(pickupEffect, transform.position, Quaternion.identity);
+            }
+
             Destroy(gameObject);
         }
     }
